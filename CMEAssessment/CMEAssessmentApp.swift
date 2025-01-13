a1d322c53de00b4ba557623d85b2825426254d93
mina.shokry-ext <mina.shokry-ext@ab-inbev.com>
2025-01-13 02:29:36 +0200

    Initial Commit

---
+++ CMEAssessment/CMEAssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMEAssessmentApp.swift
+//  CMEAssessment
+//
+//  Created by Mina Wefky on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CMEAssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
